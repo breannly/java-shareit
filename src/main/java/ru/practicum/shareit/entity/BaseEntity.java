ccc33d6a0a611717a1727e345771d263d3dc09e6
Armen Katvalyan <aakatvalyan@bk.ru>
2022-09-01 20:35:26 +0300

    feat: add base id entity

---
+++ src/main/java/ru/practicum/shareit/entity/BaseEntity.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class BaseEntity {
+    private Long id;
+}
