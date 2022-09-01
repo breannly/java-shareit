84cfdb1158167f7b89f459819834435d0527bbb0
Armen Katvalyan <aakatvalyan@bk.ru>
2022-09-01 20:41:11 +0300

    feat: add itemdto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
-public class ItemDto {
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.entity.BaseEntity;
+import ru.practicum.shareit.entity.Create;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class ItemDto extends BaseEntity {
+    @NotBlank(groups = {Create.class})
+    private String name;
+
+    @NotBlank(groups = {Create.class})
+    private String description;
+
+    @NotNull(groups = {Create.class})
+    private Boolean available;
 }
